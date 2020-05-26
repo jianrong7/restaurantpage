3f78ffd00ed56867b906c02cffd0dce08b7fd153
Loh Jian Rong <ipwnjianrong@gmail.com>
2020-05-26 21:27:13 +0800

    Added home page to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Header from './modules/header';
+import Home from './modules/home';
 
 document.addEventListener('DOMContentLoaded', () => {
   Header.setHeader();
+  Home.setHome();
 
 });
---
+++ src/modules/home.js
@@ -0,0 +1,36 @@
+const Home = (() => {
+    const setHome = () => {
+      const content = document.getElementById('content');
+  
+      const main = document.createElement('div');
+      main.classList.add('home');
+  
+      const div1 = document.createElement('div');
+
+      div1.innerText = 'We offer the best';
+  
+      const div2 = document.createElement('div');
+      div2.setAttribute('id', 'highlights');
+      div2.innerHTML = "korean. japanese. fusion.";
+  
+      const button = document.createElement('button');
+      button.setAttribute('id', 'viewMenu');
+      button.setAttribute('type', 'button')
+      button.innerText = 'View our menu';
+  
+      main.appendChild(div1);
+      main.appendChild(div2);
+      main.appendChild(button);
+  
+      content.appendChild(main);
+    };
+  
+    const setHomeActive = () => {
+      const liHome = document.getElementById('home');
+      liHome.classList.add('active');
+    };
+  
+    return { setHome, setHomeActive };
+  })();
+  
+  export default Home;
