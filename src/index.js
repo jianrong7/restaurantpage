a1aa536cc936b78826d50c65b2c646ebaffaea38
Loh Jian Rong <ipwnjianrong@gmail.com>
2020-05-26 22:14:27 +0800

    Added menu div

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Header from './modules/header';
 import Home from './modules/home';
+import Menu from './modules/menu';
 
 document.addEventListener('DOMContentLoaded', () => {
   Header.setHeader();
   Home.setHome();
+  Menu.setMenu();
 
 });
