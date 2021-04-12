0d7fd58787ab95c1be1a230af7a0c17dd6f3aea5
jianrong7 <ipwnjianrong@gmail.com>
2021-04-12 16:27:39 +0800

    Hard coded menu page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import { pageLoad } from "./pageLoad"
-// import { renderHomePage } from "./home"
-// // import { renderMenuPage } from "./menu"
+import { pageLoad } from "./pageLoad"
+import { renderHomePage } from "./home"
+import { renderMenuPage } from "./menu"
 // // import { renderContactPage } from "./contact"
 
 // const navCells = document.querySelectorAll('.nav')
---
+++ src/menu.js
@@ -0,0 +1,56 @@
+const renderMenuPage = (() => {
+    const contentContainer = document.querySelector('#content');
+    const menu = document.createElement('div');
+    menu.innerHTML = `
+    <img src="assets/donutMenu.png" id="menu">
+    <table class="menuTable">
+        <tr>
+            <th>Yeast Donuts</th>
+            <th>Vanilla Cake Donuts</th>
+            <th>Chocolate Cake Donuts</th>
+        </tr>
+        <tr>
+            <td class="dataTitle">Honey Dip</td>
+            <td class="dataTitle">Home Cut</td>
+            <td class="dataTitle">Chocolate</td>
+        </tr>
+        <tr>
+            <td class="dataDes">Our most popular donut</td>
+            <td class="dataDes">Plain vanilla cake</td>
+            <td class="dataDes">Plain chocolate cake dipped in our famous honey glaze</td>
+        </tr>
+        <tr>
+            <td class="dataTitle">Sugar Raised</td>
+            <td class="dataTitle">Honey Dew</td>
+            <td class="dataTitle">German Chocolate</td>
+        </tr>
+        <tr>
+            <td class="dataDes">Dusted in sugar</td>
+            <td class="dataDes">Plain vanilla cake dipped in our famous honey glaze</td>
+            <td class="dataDes">Fine sweetened coconut</td>
+        </tr>
+        <tr>
+            <td class="dataTitle">Jelly Filled</td>
+            <td class="dataTitle">Cinnamon</td>
+            <td class="dataTitle">Dutch Chocolate</td>
+        </tr>
+        <tr>
+            <td class="dataDes">Raspberry jam</td>
+            <td class="dataDes">Dusted in cinnamon</td>
+            <td class="dataDes">Dusted in our homemade cake crumb & cinnamon sugar topping</td>
+        </tr>
+        <tr>
+            <td class="dataTitle">Blueberry Filled</td>
+            <td class="dataTitle">Powdered</td>
+            <td class="dataTitle">Double Chocolate</td>
+        </tr>
+        <tr>
+            <td class="dataDes">Blueberry jam</td>
+            <td class="dataDes">Dusted in powedered sugar</td>
+            <td class="dataDes">Chocolate cake with chocolate icing</td>
+        </tr>
+    </table>`
+
+    contentContainer.appendChild(home)
+})();
+export { renderHomePage }
